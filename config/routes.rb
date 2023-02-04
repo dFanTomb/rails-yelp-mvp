807aadbe99307623aac8d66084d0297b37157b2f
Darko Iliev <darkoilievsum@gmail.com>
2023-02-04 21:20:27 +0100

    show restaurants_controller, restaurants view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'restaurants', to: 'restaurants#index'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # post 'reviews/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :create, :new, :show] do
+    resources :reviews, only: [:create]
+  end
 end
--- db/migrate/20230204170427_add_foreign_key_to_restaurants.rb
+++ db/migrate/20230204170427_add_foreign_key_to_restaurants.rb
@@ -3,3 +3,9 @@ class AddForeignKeyToRestaurants < ActiveRecord::Migration[7.0]
     add_reference :reviews, :restaurant, index: true
   end
 end
+
+# Nesting resources
+# rails g migration AddForeignKeyToRestaurants
+# generates THIS migration file
+# line 3 = creates restaurant_id in the schema.rb (line 29)
+# and connects restaurants table with reviews table by restaurant_id
